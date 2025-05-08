9853f3704d8a85cd21f8302de340f2d21308fbd4
alihassanml <alihassancsml@gmail.com>
2025-05-08 21:40:13 +0500

    github action

--- test/test.py
+++ test/test.py
@@ -1,14 +1,13 @@
-from src.math_operation import add,sub,mul
+from src.math_operation import add, sub, mul
 
-def addition():
-    assert add(3,2) == 5
-    assert add(4,2) == 6
+def test_addition():
+    assert add(3, 2) == 5
+    assert add(4, 2) == 6
 
-def subtracting():
-    assert sub(5,2) == 3
-    assert sub(8,2) == 6
+def test_subtracting():
+    assert sub(5, 2) == 3
+    assert sub(8, 2) == 6
 
-
-def multiplication():
-    assert mul(5,2) == 10
-    assert mul(8,2) == 16
+def test_multiplication():
+    assert mul(5, 2) == 10
+    assert mul(8, 2) == 16
